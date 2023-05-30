65c98f36b729854f874a9c7a56da4917d03e7bf4
DianeShin <jadore845@snu.ac.kr>
2023-05-30 11:25:07 +0900

    create AVL, hashtable

--- HW5/Matching.java
+++ HW5/Matching.java
@@ -2,8 +2,145 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
+class Node {
+    String key;
+    int height;
+    Node left;
+    Node right;
+
+    Node(String key) {
+        this.key = key;
+        height = 1;
+    }
+}
+
+class AVLTree {
+    private Node root;
+
+    private int height(Node node) {
+        if (node == null)
+            return 0;
+        return node.height;
+    }
+
+    private int balanceFactor(Node node) {
+        if (node == null)
+            return 0;
+        return height(node.left) - height(node.right);
+    }
+
+    private Node rotateLeft(Node z) {
+        Node y = z.right;
+        Node T2 = y.left;
+
+        y.left = z;
+        z.right = T2;
+
+        z.height = Math.max(height(z.left), height(z.right)) + 1;
+        y.height = Math.max(height(y.left), height(y.right)) + 1;
+
+        return y;
+    }
+
+    private Node rotateRight(Node z) {
+        Node y = z.left;
+        Node T2 = y.right;
+
+        y.right = z;
+        z.left = T2;
+
+        z.height = Math.max(height(z.left), height(z.right)) + 1;
+        y.height = Math.max(height(y.left), height(y.right)) + 1;
+
+        return y;
+    }
+
+    private Node insert(Node node, String key) {
+        if (node == null)
+            return new Node(key);
+
+        if (key.compareTo(node.key) < 0)
+            node.left = insert(node.left, key);
+        else if (key.compareTo(node.key) > 0)
+            node.right = insert(node.right, key);
+        else
+            return node;
+
+        node.height = 1 + Math.max(height(node.left), height(node.right));
+
+        int balance = balanceFactor(node);
+
+        if (balance > 1 && key.compareTo(node.left.key) < 0)
+            return rotateRight(node);
+
+        if (balance < -1 && key.compareTo(node.right.key) > 0)
+            return rotateLeft(node);
+
+        if (balance > 1 && key.compareTo(node.left.key) > 0) {
+            node.left = rotateLeft(node.left);
+            return rotateRight(node);
+        }
+
+        if (balance < -1 && key.compareTo(node.right.key) < 0) {
+            node.right = rotateRight(node.right);
+            return rotateLeft(node);
+        }
+
+        return node;
+    }
+
+    public void insert(String key) {
+        root = insert(root, key);
+    }
+
+    private void inorderTraversal(Node node, List<String> result) {
+        if (node != null) {
+            inorderTraversal(node.left, result);
+            result.add(node.key);
+            inorderTraversal(node.right, result);
+        }
+    }
+
+    public List<String> inorderTraversal() {
+        List<String> result = new ArrayList<>();
+        inorderTraversal(root, result);
+        return result;
+    }
+}
+
+class HashTable {
+    private AVLTree[] slots;
+
+    public HashTable() {
+        slots = new AVLTree[100];
+        for (int i = 0; i < 100; i++) {
+            slots[i] = new AVLTree();
+        }
+    }
+
+    private int hash(String key) {
+        int sum = 0;
+        for (int i = 0; i < key.length(); i++) {
+            sum += (int) key.charAt(i);
+        }
+        return sum % 100;
+    }
+
+    public void insert(String key) {
+        int slotIndex = hash(key);
+        slots[slotIndex].insert(key);
+    }
+
+    public List<String> search(String key) {
+        int slotIndex = hash(key);
+        return slots[slotIndex].inorderTraversal();
+    }
+}
+
 public class Matching
 {
+	static HashTable hashTable = new HashTable();
+
 	public static void main(String args[])
 	{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -31,12 +168,10 @@ public class Matching
 		String filePath = input.split(" ")[1];
 
 		if (command == '<'){
-			List<String> lines = new ArrayList<>();
-
 			try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
 				String line;
 				while ((line = reader.readLine()) != null) {
-					lines.add(line);
+					hashTable.insert(line);
 				}
 			} catch (IOException e) {
 				System.err.println("Error reading the file: " + e.getMessage());
