ebeb63b8753364b8e7e623d433b51eed2fa173c6
DianeShin <jadore845@snu.ac.kr>
2023-06-01 22:22:19 +0900

    implement ?

--- HW5/Matching.java
+++ HW5/Matching.java
@@ -1,20 +1,60 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Matching
 {
-    static class Node {
+    static class Tuple<T1, T2>{
+        private final T1 item1;
+        private final T2 item2;
+    
+        public Tuple(T1 item1, T2 item2) {
+            this.item1 = item1;
+            this.item2 = item2;
+        }
+
+        public T1 getItem1() {
+            return item1;
+        }
+    
+        public T2 getItem2() {
+            return item2;
+        }
+    
+        @Override
+        public String toString() {
+            return "(" + item1 + ", " + item2 + ")";
+        }
+    }
+
+    static class Node implements Comparable<Node>{
         String key;
         int height;
-        int line;
+        Tuple<Integer, Integer> location;
         Node left;
         Node right;
     
-        Node(String key, int line) {
+        Node(String key, Tuple<Integer, Integer> location) {
             this.key = key;
-            this.line = line;
+            this.location = location;
             height = 1;
+        }
+
+        @Override
+        public int compareTo(Node otherNode) {      
+            if (this.key.compareTo(otherNode.key) > 0) return 1;
+            else if (this.key.compareTo(otherNode.key) < 0) return -1;
+            else{
+                if (this.location.item1 > otherNode.location.item1) return 1;
+                else if (this.location.item1 < otherNode.location.item1) return -1;
+                else{
+                    if (this.location.item2 > otherNode.location.item2) return 1;
+                    else if (this.location.item2 < otherNode.location.item2) return -1;
+                    else return 0;
+                }
+            }
         }
     }
     
@@ -59,14 +99,17 @@ public class Matching
             return y;
         }
     
-        private Node insert(Node node, String key, int line) {
+        // TODO ; ensure uniqueness of node.
+        private Node insert(Node node, String key, Tuple<Integer, Integer> location) {
+            Node tempNode = new Node(key, location);
+
             if (node == null)
-                return new Node(key, line);
+                return new Node(key, location);
     
-            if (key.compareTo(node.key) < 0)
-                node.left = insert(node.left, key, line);
-            else if (key.compareTo(node.key) > 0)
-                node.right = insert(node.right, key, line);
+            if (tempNode.compareTo(node) < 0)
+                node.left = insert(node.left, key, location);
+            else if (tempNode.compareTo(node) > 0)
+                node.right = insert(node.right, key, location);
             else
                 return node;
     
@@ -74,18 +117,18 @@ public class Matching
     
             int balance = balanceFactor(node);
     
-            if (balance > 1 && key.compareTo(node.left.key) < 0)
+            if (balance > 1 && tempNode.compareTo(node.left) < 0)
                 return rotateRight(node);
     
-            if (balance < -1 && key.compareTo(node.right.key) > 0)
+            if (balance < -1 && tempNode.compareTo(node.right) > 0)
                 return rotateLeft(node);
     
-            if (balance > 1 && key.compareTo(node.left.key) > 0) {
+            if (balance > 1 && tempNode.compareTo(node.left) > 0) {
                 node.left = rotateLeft(node.left);
                 return rotateRight(node);
             }
     
-            if (balance < -1 && key.compareTo(node.right.key) < 0) {
+            if (balance < -1 && tempNode.compareTo(node.right) < 0) {
                 node.right = rotateRight(node.right);
                 return rotateLeft(node);
             }
@@ -93,8 +136,8 @@ public class Matching
             return node;
         }
     
-        public void insert(String key, int line) {
-            root = insert(root, key, line);
+        public void insert(String key, Tuple<Integer, Integer> location) {
+            root = insert(root, key, location);
         }
 
         private void preorderTraversal(Node node, List<String> result) {
@@ -108,6 +151,20 @@ public class Matching
         public List<String> preorderTraversal() {
             List<String> result = new ArrayList<>();
             preorderTraversal(root, result);
+            return result;
+        }
+
+        private void preorderTraversalSearch(Node node,  List<Tuple<Integer,Integer>> result, String substr) {
+            if (node != null) {
+                if (node.key.equals(substr)) result.add(node.location);
+                preorderTraversalSearch(node.left, result, substr);
+                preorderTraversalSearch(node.right, result, substr);
+            }
+        }
+        
+        public List<Tuple<Integer,Integer>> preorderTraversalSearch(String substr) {
+            List<Tuple<Integer,Integer>> result = new ArrayList<>();
+            preorderTraversalSearch(root, result, substr);
             return result;
         }
     }
@@ -135,7 +192,7 @@ public class Matching
             while (startIndex + 5 < key.length()){
                 String substring = key.substring(startIndex, startIndex + 6);
                 int slotIndex = hash(substring);
-                slots[slotIndex].insert(substring, line);
+                slots[slotIndex].insert(substring, new Tuple<Integer, Integer>(line, startIndex+1));
                 startIndex++;                
             }
         }               
@@ -143,10 +200,50 @@ public class Matching
         public List<String> search(int slot) {
             return slots[slot].preorderTraversal();
         }
-    
-        // TODO
-        public void findString(String input) {
-            ;
+
+        public List<Tuple<Integer,Integer>> searchSubstr(String substr) {
+            return slots[hash(substr)].preorderTraversalSearch(substr);
+        }
+
+        public List<Tuple<Integer, Integer>> findString(String pattern) {
+            List<String> substrList = new ArrayList<>();
+            List<List<Tuple<Integer,Integer>>> searchResult = new ArrayList<>();
+            List<Tuple<Integer, Integer>> result = new ArrayList<>();
+            int startIndex = 0;
+            System.out.println(pattern.length());
+            // 1. split pattern with 6 char.
+            while (startIndex < pattern.length()){
+               substrList.add(pattern.substring(startIndex, Math.min(startIndex + 6, pattern.length())));
+               startIndex += 6;
+            }
+            for (String iter : substrList) System.out.println(iter);
+            // 2. search location of every pattern
+            for (String substr : substrList){
+                searchResult.add(hashTable.searchSubstr(substr));
+            }
+            
+            // 3. if connects, we can fetch first search result.
+            for(Tuple<Integer,Integer> tupleIter :searchResult.get(0)){ // iterating starting index
+                boolean fail = false;
+
+                for (int index = 1; index < searchResult.size(); index++){ // access each block of pattern substr
+                    if (fail) break; // search fail -> move to next starter location
+                    else{
+                        boolean succ = false;
+                        for (Tuple<Integer, Integer> innerTupleIter : searchResult.get(index)){
+                            if (tupleIter.item1 == innerTupleIter.item1 && tupleIter.item2 + 6 == innerTupleIter.item2){
+                                succ = true;
+                                break; // search success for now.
+                            } 
+                            else continue; // mismatch
+                        }  
+                        if (!succ) fail = true; // search fail        
+                    }
+                }      
+                if (!fail) result.add(tupleIter);   
+            }
+
+            return result;
         }
     }
 
@@ -177,7 +274,7 @@ public class Matching
 	private static void command(String input)
 	{
 		char command = input.charAt(0);
-		String arg = input.split(" ")[1];
+		String arg = input.substring(2);
 
 		if (command == '<'){
 			// 1. init values
@@ -206,8 +303,31 @@ public class Matching
 		}
 
 		else if (command == '?'){
-			
+            // find search result
+			List<Tuple<Integer,Integer>> searchResult = hashTable.findString(arg);
+
+            // Create a custom comparator to compare the tuples
+            Comparator<Tuple<Integer, Integer>> tupleComparator = new Comparator<Tuple<Integer, Integer>>() {
+                @Override
+                public int compare(Tuple<Integer, Integer> tuple1, Tuple<Integer, Integer> tuple2) {
+                    int item1Comparison = tuple1.getItem1().compareTo(tuple2.getItem1());
+                    if (item1Comparison != 0) {
+                        return item1Comparison; // Compare item1 first
+                    }
+                    return tuple1.getItem2().compareTo(tuple2.getItem2()); // Compare item2 if item1 is identical
+                }
+            };
+
+            // Sort the searchResult list using the custom comparator
+            Collections.sort(searchResult, tupleComparator);
+            
+            if (searchResult.isEmpty()) System.out.print("(0, 0)");
+            else{
+                for (Tuple<Integer, Integer> tup : searchResult) System.out.print(tup.toString() + ' ');
+            }
+            System.out.println();
 		}
+
 		else if (command == '/');
 		else if (command == '+'){
 			hashTable.insert(arg, lineNumber);
